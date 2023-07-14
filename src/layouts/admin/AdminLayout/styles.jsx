cc3a923f03b872bb72b4cd53cf06ed3d2d51702b
quangdung861 <quangdung861@gmail.com>
2023-07-14 12:07:09 +0700

    [#02] Update scrollbar with position: sticky.

--- src/layouts/admin/AdminLayout/styles.jsx
+++ src/layouts/admin/AdminLayout/styles.jsx
@@ -4,7 +4,6 @@ export const MainContainer = styled.div`
   max-width: 1380px;
   width: 100%;
   margin: 0 auto;
-  overflow-x: visible;
 `;
 export const MainContent = styled.div`
   min-height: 100vh;
